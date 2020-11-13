6025466d62e15b6028c41e1ac0e83b281548324f
Govardhan Isayamudhan Naicker <62525512+govardhan1194@users.noreply.github.com>
2020-11-12 18:43:38 -0800

    Update consumerkafka.py

--- consumerkafka.py
+++ consumerkafka.py
@@ -1,12 +1,19 @@
 from kafka import KafkaConsumer
 import json
 
+def json_deserializer(data):
+    return json.loads(decode('utf-8'))
+
 if __name__ == "__main__":
     consumer = KafkaConsumer(
         "test",
         bootstrap_servers='localhost:9092',
         auto_offset_reset='earliest',
-        group_id="consumer-group-a")
+        group_id="consumer-group-a",
+        value_deserializer=json_deserializer)
     print("starting the consumer")
     for msg in consumer:
         print("Registered User = {}".format(json.loads(msg.value)))
+
+        
+      
